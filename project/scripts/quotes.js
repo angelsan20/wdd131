e20ce52f3e0799adc904853854c29caefd633d9a
angelsan20 <san21103@byui.edu>
2024-10-31 23:26:38 -0400

    submission of the final project to save changes.

--- project/scripts/quotes.js
+++ project/scripts/quotes.js
@@ -71,24 +71,6 @@ const quotes = [
         image: "https://www.churchofjesuschrist.org/imgs/71387a567147fb0080baa99b008dcb54099e9eef/full/!320%2C400/0/default",
         alt: "L. Whitney Clayton"
     },
-    {
-        quote: "Marriage is a gift from God to us; the quality of our marriages is a gift from us to Him.",
-        speaker: "L. Whitney Clayton",
-        image: "https://www.churchofjesuschrist.org/imgs/71387a567147fb0080baa99b008dcb54099e9eef/full/!320%2C400/0/default",
-        alt: "L. Whitney Clayton"
-    },
-    {
-        quote: "Marriage is a gift from God to us; the quality of our marriages is a gift from us to Him.",
-        speaker: "L. Whitney Clayton",
-        image: "https://www.churchofjesuschrist.org/imgs/71387a567147fb0080baa99b008dcb54099e9eef/full/!320%2C400/0/default",
-        alt: "L. Whitney Clayton"
-    },
-    {
-        quote: "Marriage is a gift from God to us; the quality of our marriages is a gift from us to Him.",
-        speaker: "L. Whitney Clayton",
-        image: "https://www.churchofjesuschrist.org/imgs/71387a567147fb0080baa99b008dcb54099e9eef/full/!320%2C400/0/default",
-        alt: "L. Whitney Clayton"
-    },
 ];
 
 function getRandomQuote() {
@@ -102,8 +84,8 @@ function displayQuote() {
     
     quoteSection.innerHTML = `
         <img src="${quoteData.image}" alt="${quoteData.alt}">
-        <h2>"${quoteData.quote}"</h2>
-        <p>${quoteData.speaker}</p>
+        <p class="p-quote">"${quoteData.quote}"</p>
+        <p class="p-speaker">${quoteData.speaker}</p>
     `;
 }
 
